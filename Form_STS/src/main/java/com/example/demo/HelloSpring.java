1e9bb26ebd49c47334285de6837e7ccde2121746
Takeno90 <takehiro.moro@gmail.com>
2021-11-14 19:11:49 +0900

    機能追加、仕様変更 view index.htmlのみで再帰的に使う

---
+++ Form_STS/src/main/java/com/example/demo/HelloSpring.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloSpring {
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String getIndex(Model model) {
+		model.addAttribute("th_greet", "こんにちは！氏名を入力ください");
+		return "index";
+	}
+
+	@RequestMapping(value="/",method=RequestMethod.POST)
+	public String postIndex(@RequestParam("nickname")String nickname,
+							@RequestParam("radio_1")String sex,
+							@RequestParam("favorite_distribution")String distribution,
+							Model model) {
+		String sexuality = sex.equals("maile") ? "君" : "さん";
+		String message = "こんにちは！" + nickname + sexuality + "の好きなディストリビューションは" + distribution + "ですね。";
+		model.addAttribute("th_greet",message);
+		return "index";
+	}
+
+
+}
